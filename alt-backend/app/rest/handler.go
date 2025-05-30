95922be3ba6396a83c6bbbe30888fcc31480dd75
Kaikei <e_kaikei@protonmail.com>
2025-05-30 23:17:14 +0900

    Add health check endpoint in REST handler using Echo framework

---
+++ alt-backend/app/rest/handler.go
@@ -0,0 +1,17 @@
+package rest
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterRoutes(e *echo.Echo) {
+	v1 := e.Group("/v1")
+	v1.GET("/health", func(c echo.Context) error {
+		response := map[string]string{
+			"status": "healthy",
+		}
+		return c.JSON(http.StatusOK, response)
+	})
+}
+
