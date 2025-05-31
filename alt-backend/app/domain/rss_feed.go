c7a5dd6dccc6ac8ffa497cf528d0c841d4e31b65
Kaikei <e_kaikei@protonmail.com>
2025-05-31 11:30:39 +0900

    Add RSSFeed data structures to support RSS feed parsing and management, including types for feed items, images, and extensions.

---
+++ alt-backend/app/domain/rss_feed.go
@@ -0,0 +1,110 @@
+package domain
+
+import "time"
+
+type RSSFeed struct {
+	Title         string            `json:"title"`
+	Description   string            `json:"description"`
+	Link          string            `json:"link"`
+	FeedLink      string            `json:"feedLink"`
+	Links         []string          `json:"links"`
+	Updated       string            `json:"updated"`
+	UpdatedParsed time.Time         `json:"updatedParsed"`
+	Language      string            `json:"language"`
+	Image         RSSFeedImage      `json:"image"`
+	Generator     string            `json:"generator"`
+	Extensions    RSSFeedExtensions `json:"extensions"`
+	Items         []Item            `json:"items"`
+	FeedType      string            `json:"feedType"`
+	FeedVersion   string            `json:"feedVersion"`
+}
+
+type RSSFeedExtensions struct {
+	Atom Atom `json:"atom"`
+}
+
+type Atom struct {
+	Link []Link `json:"link"`
+}
+
+type Link struct {
+	Name     string   `json:"name"`
+	Value    string   `json:"value"`
+	Attrs    Attrs    `json:"attrs"`
+	Children Children `json:"children"`
+}
+
+type Attrs struct {
+	Href string `json:"href"`
+	Rel  string `json:"rel"`
+	Type string `json:"type"`
+}
+
+type Children struct {
+}
+
+type RSSFeedImage struct {
+	URL   string `json:"url"`
+	Title string `json:"title"`
+}
+
+type Item struct {
+	Title           string         `json:"title"`
+	Description     string         `json:"description"`
+	Link            string         `json:"link"`
+	Links           []string       `json:"links"`
+	Published       string         `json:"published"`
+	PublishedParsed time.Time      `json:"publishedParsed"`
+	Author          Author         `json:"author"`
+	Authors         []Author       `json:"authors"`
+	GUID            string         `json:"guid"`
+	Image           ItemImage      `json:"image"`
+	Enclosures      []Enclosure    `json:"enclosures"`
+	DcEXT           DcEXT          `json:"dcExt"`
+	Extensions      ItemExtensions `json:"extensions"`
+}
+
+type Author struct {
+	Name string `json:"name"`
+}
+
+type DcEXT struct {
+	Creator []string `json:"creator"`
+}
+
+type Enclosure struct {
+	URL    string `json:"url"`
+	Length string `json:"length"`
+	Type   Type   `json:"type"`
+}
+
+type ItemExtensions struct {
+	Dc Dc `json:"dc"`
+}
+
+type Dc struct {
+	Creator []CreatorElement `json:"creator"`
+}
+
+type CreatorElement struct {
+	Name     Name     `json:"name"`
+	Value    string   `json:"value"`
+	Attrs    Children `json:"attrs"`
+	Children Children `json:"children"`
+}
+
+type ItemImage struct {
+	URL string `json:"url"`
+}
+
+type Type string
+
+const (
+	ImagePNG Type = "image/png"
+)
+
+type Name string
+
+const (
+	Creator Name = "creator"
+)
