79bb204160cfc14ec1b3cb6cd18f96b4b8a10ca6
Kaikei <e_kaikei@protonmail.com>
2025-06-04 15:41:37 +0900

    RegisterSingleFeedおよびRegisterRSSFeedLinkメソッドにトランザクション処理を追加し、エラーハンドリングを改善

--- alt-backend/app/driver/alt_db/register_feeds_driver.go
+++ alt-backend/app/driver/alt_db/register_feeds_driver.go
@@ -4,15 +4,29 @@ import (
 	"alt/driver/models"
 	"alt/utils/logger"
 	"context"
+	"errors"
 )
 
 func (r *AltDBRepository) RegisterSingleFeed(ctx context.Context, feed *models.Feed) error {
-	_, err := r.db.Exec(ctx, "INSERT INTO feeds (title, description, link, pub_date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (link) DO NOTHING", feed.Title, feed.Description, feed.Link, feed.PubDate, feed.CreatedAt, feed.UpdatedAt)
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
+		logger.Logger.Error("Error starting transaction", "error", err)
+		return errors.New("error starting transaction")
+	}
+	defer tx.Rollback(ctx)
+
+	_, err = tx.Exec(ctx, "INSERT INTO feeds (title, description, link, pub_date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (link) DO NOTHING", feed.Title, feed.Description, feed.Link, feed.PubDate, feed.CreatedAt, feed.UpdatedAt)
 	if err != nil {
 		logger.Logger.Error("Error registering single feed link", "error", err)
-		return err
+		return errors.New("error registering single feed link")
 	}
 	logger.Logger.Info("Single feed link registered", "link", feed.Link)
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		logger.Logger.Error("Error committing transaction", "error", err)
+		return errors.New("error committing transaction")
+	}
 
 	return nil
 }
@@ -22,7 +36,7 @@ func (r *AltDBRepository) RegisterMultipleFeeds(ctx context.Context, feeds []mod
 		err := r.RegisterSingleFeed(ctx, &feed)
 		if err != nil {
 			logger.Logger.Error("Error registering multiple feeds", "error", err)
-			return err
+			return errors.New("error registering multiple feeds")
 		}
 	}
 	return nil
--- alt-backend/app/driver/alt_db/register_rss_feed_link_driver.go
+++ alt-backend/app/driver/alt_db/register_rss_feed_link_driver.go
@@ -3,13 +3,21 @@ package alt_db
 import (
 	"alt/utils/logger"
 	"context"
+	"errors"
 )
 
 func (r *AltDBRepository) RegisterRSSFeedLink(ctx context.Context, link string) error {
-	_, err := r.db.Exec(ctx, "INSERT INTO feed_links (url) VALUES ($1)", link)
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
+		logger.Logger.Error("Error starting transaction", "error", err)
+		return errors.New("error starting transaction")
+	}
+	defer tx.Rollback(ctx)
+
+	_, err = tx.Exec(ctx, "INSERT INTO feed_links (url) VALUES ($1)", link)
 	if err != nil {
 		logger.Logger.Error("Error registering RSS feed link", "error", err)
-		return err
+		return errors.New("error registering RSS feed link")
 	}
 
 	logger.Logger.Info("RSS feed link registered", "link", link)
