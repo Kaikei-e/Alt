485caec8d01ce1c61b63e279076c23a171656847
Kaikei <e_kaikei@protonmail.com>
2025-07-09 13:37:57 +0900

    Add Desktop Articles Page and Timeline Component
    - Introduced a new DesktopArticlesPage component that utilizes Suspense for loading state management.
    - Created DesktopArticleTimeline component to display article timelines.
    - Implemented a loading fallback UI for better user experience during data fetching.

---
+++ alt-frontend/app/src/components/desktop/timeline/DesktopArticleTimeline.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const DesktopArticleTimeline = () => {
+
+  return (
+    <Text>DesktopArticleTimeline</Text>
+  );
+}
+
+export { DesktopArticleTimeline };
